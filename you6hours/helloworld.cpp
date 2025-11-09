1e9e8b02ed0ab747c5c62410515559c2c75498bf
Symbat Dulatuly <SDulatuly@icore.kz>
2025-11-09 11:17:53 +0500

    const keyword

--- you6hours/helloworld.cpp
+++ you6hours/helloworld.cpp
@@ -1,22 +1,10 @@
 #include <iostream>
 
 int main(){
-    int x; //decleration
-    x = 5; //assignment
-    int y = 6;
-    int sum = x + y;
-    std::cout << x << std::endl;
-    std::cout << y << std::endl;
-    std::cout << sum << std::endl;
-    //integer (whole number) approximately 4 bytes from -2billion to +2 billions
-    //double (including decimal) approximately 8 bytes precion is about 15 to 16 decimal digits
-    double temperature = 25.1;
-    std::cout << temperature << std::endl;
-    char grade = 'A';
-    std::cout << grade<<'\n';
-    bool is_student = false;
-    std::cout << is_student << std::endl;
-    std::string name = "Symbat";
-    std::cout << name << std::endl;
+    //const keyword makes only read only. Protects from changing
+    const double PI = 3.14159;
+    double radius = 10;
+    double circumference = 2 * PI * radius;
+    std::cout << circumference <<" cm"<< std::endl;
     return 0;
 }
