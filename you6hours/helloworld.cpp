4931dd21bfaeabaebe57bfd48bf004f35a41de85
Symbat Dulatuly <SDulatuly@icore.kz>
2025-11-09 10:58:36 +0500

    First lesson from youtube 6 hour video

---
+++ you6hours/helloworld.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main(){
+    std::cout << "I like pizza!" << '\n';
+    std::cout << "It is really good !" << '\n';
+    // this is comment
+    /*THis 
+    is 
+    multi-line
+    */
+    return 0;
+}
