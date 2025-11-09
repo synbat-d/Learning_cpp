36b4ff217aca12cf394553cca8863440d1884c3e
Symbat Dulatuly <SDulatuly@icore.kz>
2025-11-09 11:13:43 +0500

    lesson 2 common data types

--- you6hours/helloworld.cpp
+++ you6hours/helloworld.cpp
@@ -1,12 +1,22 @@
 #include <iostream>
 
 int main(){
-    std::cout << "I like pizza!" << '\n';
-    std::cout << "It is really good !" << '\n';
-    // this is comment
-    /*THis 
-    is 
-    multi-line
-    */
+    int x; //decleration
+    x = 5; //assignment
+    int y = 6;
+    int sum = x + y;
+    std::cout << x << std::endl;
+    std::cout << y << std::endl;
+    std::cout << sum << std::endl;
+    //integer (whole number) approximately 4 bytes from -2billion to +2 billions
+    //double (including decimal) approximately 8 bytes precion is about 15 to 16 decimal digits
+    double temperature = 25.1;
+    std::cout << temperature << std::endl;
+    char grade = 'A';
+    std::cout << grade<<'\n';
+    bool is_student = false;
+    std::cout << is_student << std::endl;
+    std::string name = "Symbat";
+    std::cout << name << std::endl;
     return 0;
 }
